f2027c05cf2fd11336ddd39b6e288a9e5445c3e6
Swetha Vadlakonda <swethv@google.com>
2022-12-10 16:03:08 +0000

    removing retry bucket

--- vendor/github.com/jacobsa/gcloud/gcs/conn.go
+++ vendor/github.com/jacobsa/gcloud/gcs/conn.go
@@ -161,10 +161,10 @@ func (c *conn) OpenBucket(
 
 	// Enable retry loops if requested.
 	// Enable retry loops if requested.
-	if c.maxBackoffSleep > 0 {
+	/*if c.maxBackoffSleep > 0 {
 		// TODO(jacobsa): Show the retries as distinct spans in the trace.
 		b = newRetryBucket(c.maxBackoffSleep, b)
-	}
+	}*/
 
 	// Enable tracing if appropriate.
 	if reqtrace.Enabled() {
