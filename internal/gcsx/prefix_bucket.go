5532774b2def80cfa64d1fc3d530d8c3df6379b2
Swetha Vadlakonda <swethv@google.com>
2022-12-12 05:29:01 +0000

    invoking reader http call from prefix bucket

--- internal/gcsx/prefix_bucket.go
+++ internal/gcsx/prefix_bucket.go
@@ -16,12 +16,18 @@ package gcsx
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"math"
 	"net/http"
+	"net/url"
 	"strings"
 	"unicode/utf8"
 
+	"github.com/jacobsa/gcloud/httputil"
 	"golang.org/x/net/context"
+	"google.golang.org/api/googleapi"
 
 	"github.com/jacobsa/gcloud/gcs"
 )
@@ -77,7 +83,113 @@ func (b *prefixBucket) NewReader(
 	*mReq = *req
 	mReq.Name = b.wrappedName(req.Name)
 
-	rc, err = b.wrapped.NewReader(ctx, mReq)
+	fmt.Println("Invoking from prefix bucket")
+	bucketSegment := httputil.EncodePathSegment("swethv-test-central")
+	objectSegment := httputil.EncodePathSegment(req.Name)
+	opaque := fmt.Sprintf(
+		"//%s/download/storage/v1/b/%s/o/%s",
+		"storage.googleapis.com:443",
+		bucketSegment,
+		objectSegment)
+
+	query := make(url.Values)
+	query.Set("alt", "media")
+
+	if req.Generation != 0 {
+		query.Set("generation", fmt.Sprintf("%d", req.Generation))
+	}
+
+	url := &url.URL{
+		Scheme:   "https",
+		Host:     "storage.googleapis.com:443",
+		Opaque:   opaque,
+		RawQuery: query.Encode(),
+	}
+
+	// Create an HTTP request.
+	httpReq, err := httputil.NewRequest(ctx, "GET", url, nil, 0, "test")
+	if err != nil {
+		err = fmt.Errorf("httputil.NewRequest: %v", err)
+		return
+	}
+
+	if req.Range != nil {
+		var v string
+		v, _ = makeRangeHeaderValue(*req.Range)
+		httpReq.Header.Set("Range", v)
+	}
+
+	// Call the server.
+	httpRes, err := b.wrapped.GetHttpClient().Do(httpReq)
+	if err != nil {
+		return
+	}
+
+	// Close the body if we're returning in error.
+	defer func() {
+		if err != nil {
+			googleapi.CloseBody(httpRes)
+		}
+	}()
+
+	// Check for HTTP error statuses.
+	if err = googleapi.CheckResponse(httpRes); err != nil {
+		if typed, ok := err.(*googleapi.Error); ok {
+			// Special case: handle not found errors.
+			if typed.Code == http.StatusNotFound {
+				err = &gcs.NotFoundError{Err: typed}
+			}
+
+			// Special case: if the user requested a range and we received HTTP 416
+			// from the server, treat this as an empty body. See makeRangeHeaderValue
+			// for more details.
+			if req.Range != nil &&
+				typed.Code == http.StatusRequestedRangeNotSatisfiable {
+				err = nil
+				googleapi.CloseBody(httpRes)
+				rc = ioutil.NopCloser(strings.NewReader(""))
+			}
+		}
+
+		return
+	}
+
+	// The body contains the object data.
+	rc = httpRes.Body
+
+	return
+}
+
+func makeRangeHeaderValue(br gcs.ByteRange) (hdr string, n int64) {
+	// HACK(jacobsa): Above a certain number N, GCS appears to treat Range
+	// headers containing a last-byte-pos > N as syntactically invalid. I've
+	// experimentally determined that N is 2^63-1, which makes sense if they are
+	// using signed integers.
+	//
+	// Since math.MaxUint64 is a reasonable way to express "infinity" for a
+	// limit, and because we don't intend to support eight-exabyte objects,
+	// handle this by truncating the limit. This also prevents overflow when
+	// casting to int64 below.
+	if br.Limit > math.MaxInt64 {
+		br.Limit = math.MaxInt64
+	}
+
+	// Canonicalize ranges that the server will not like. We must do this because
+	// RFC 2616 §14.35.1 requires the last byte position to be greater than or
+	// equal to the first byte position.
+	if br.Limit < br.Start {
+		br.Start = 0
+		br.Limit = 0
+	}
+
+	// HTTP byte range specifiers are [min, max] double-inclusive, ugh. But we
+	// require the user to truncate, so there is no harm in requesting one byte
+	// extra at the end. If the range GCS sees goes past the end of the object,
+	// it truncates. If the range starts after the end of the object, it returns
+	// HTTP 416, which we require the user to handle.
+	hdr = fmt.Sprintf("bytes=%d-%d", br.Start, br.Limit)
+	n = int64(br.Limit - br.Start)
+
 	return
 }
 
