5a60584f7f69672a2c4446135fa8f933959ccce8
Swetha Vadlakonda <swethv@google.com>
2023-01-02 13:53:05 +0530

    removing ad5 and acl

--- internal/storage/storageutil/object_attrs.go
+++ internal/storage/storageutil/object_attrs.go
@@ -1,6 +1,7 @@
 package storageutil
 
 import (
+	"crypto/md5"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -37,45 +38,40 @@ func convertACLRuleToObjectAccessControl(element storage.ACLRule) *storagev1.Obj
 }
 
 func ObjectAttrsToBucketObject(attrs *storage.ObjectAttrs) *gcs.Object {
-
-	return &gcs.Object{
-		Name: attrs.Name,
-		Size: uint64(attrs.Size),
-	}
 	// gcs.Object accepts []*storagev1.ObjectAccessControl instead of []ACLRule.
-	/*	var acl []*storagev1.ObjectAccessControl
-		for _, element := range attrs.ACL {
-			acl = append(acl, convertACLRuleToObjectAccessControl(element))
-		}
+	var acl []*storagev1.ObjectAccessControl
+	for _, element := range attrs.ACL {
+		acl = append(acl, convertACLRuleToObjectAccessControl(element))
+	}
 
-		// Converting MD5[] slice to MD5[md5.Size] type fixed array as accepted by GCSFuse.
-		var md5 [md5.Size]byte
-		copy(md5[:], attrs.MD5)
+	// Converting MD5[] slice to MD5[md5.Size] type fixed array as accepted by GCSFuse.
+	var md5 [md5.Size]byte
+	copy(md5[:], attrs.MD5)
 
-		// Setting the parameters in Object and doing conversions as necessary.
-		return &gcs.Object{
-			Name:            attrs.Name,
-			ContentType:     attrs.ContentType,
-			ContentLanguage: attrs.ContentLanguage,
-			CacheControl:    attrs.CacheControl,
-			Owner:           attrs.Owner,
-			Size:            uint64(attrs.Size),
-			ContentEncoding: attrs.ContentEncoding,
-			MD5:             &md5,
-			CRC32C:          &attrs.CRC32C,
-			MediaLink:       attrs.MediaLink,
-			Metadata:        attrs.Metadata,
-			Generation:      attrs.Generation,
-			MetaGeneration:  attrs.Metageneration,
-			StorageClass:    attrs.StorageClass,
-			Deleted:         attrs.Deleted,
-			Updated:         attrs.Updated,
-			//ComponentCount: , (Field not found in attrs returned by Go Client.)
-			ContentDisposition: attrs.ContentDisposition,
-			CustomTime:         string(attrs.CustomTime.Format(time.RFC3339)),
-			EventBasedHold:     attrs.EventBasedHold,
-			Acl:                acl,
-		}*/
+	// Setting the parameters in Object and doing conversions as necessary.
+	return &gcs.Object{
+		Name:            attrs.Name,
+		ContentType:     attrs.ContentType,
+		ContentLanguage: attrs.ContentLanguage,
+		CacheControl:    attrs.CacheControl,
+		Owner:           attrs.Owner,
+		Size:            uint64(attrs.Size),
+		ContentEncoding: attrs.ContentEncoding,
+		//	MD5:             &md5,
+		CRC32C:         &attrs.CRC32C,
+		MediaLink:      attrs.MediaLink,
+		Metadata:       attrs.Metadata,
+		Generation:     attrs.Generation,
+		MetaGeneration: attrs.Metageneration,
+		StorageClass:   attrs.StorageClass,
+		Deleted:        attrs.Deleted,
+		Updated:        attrs.Updated,
+		//ComponentCount: , (Field not found in attrs returned by Go Client.)
+		ContentDisposition: attrs.ContentDisposition,
+		CustomTime:         string(attrs.CustomTime.Format(time.RFC3339)),
+		EventBasedHold:     attrs.EventBasedHold,
+		//	Acl:                acl,
+	}
 }
 
 // SetAttrsInWriter - for setting object-attributes filed in storage.Writer object.
