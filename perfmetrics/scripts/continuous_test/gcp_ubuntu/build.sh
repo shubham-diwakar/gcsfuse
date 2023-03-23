1dc4203f350bdb295c5d39cc391837677afaac21
Prince Kumar <princer@google.com>
2023-03-23 03:26:26 +0000

    Adding nohup to get stdout log

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -30,18 +30,18 @@ cd "./perfmetrics/scripts/"
 echo Mounting gcs bucket
 mkdir -p gcs
 LOG_FILE=log-$(date '+%Y-%m-%d').txt
-GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
+GCSFUSE_FLAGS="--foreground --implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
 BUCKET_NAME=gcs-fuse-dashboard-fio
 MOUNT_POINT=gcs
 # The VM will itself exit if the gcsfuse mount fails.
-gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+nohup gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT 1> "gcsfuse.out" 2> "gcsfuse.err" &
 
 # Executing perf tests
 chmod +x run_load_test_and_fetch_metrics.sh
 ./run_load_test_and_fetch_metrics.sh
-## Copying gcsfuse logs to bucket
-#gsutil -m cp $LOG_FILE gs://gcs-fuse-dashboard-fio/fio-gcsfuse-logs/
-#
+# Copying gcsfuse logs to bucket
+gsutil -m cp $LOG_FILE gs://gcs-fuse-dashboard-fio/fio-gcsfuse-logs/
+
 ## Deleting logs older than 10 days
 #python3 utils/metrics_util.py gcs/fio-gcsfuse-logs/ 10
 #
