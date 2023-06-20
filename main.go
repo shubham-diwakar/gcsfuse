29db76264d6fe5c3a1aa70ad78edf82d83bc1166
Swetha Vadlakonda <swethv@google.com>
2023-06-20 10:29:54 +0530

    returning eventhough umount failed

--- main.go
+++ main.go
@@ -67,6 +67,7 @@ func registerSIGINTHandler(mountPoint string) {
 			err := fuse.Unmount(mountPoint)
 			if err != nil {
 				logger.Infof("Failed to unmount in response to SIGINT: %v", err)
+				return
 			} else {
 				logger.Infof("Successfully unmounted in response to SIGINT.")
 				return
