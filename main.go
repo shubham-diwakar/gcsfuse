b07b0b3a3d92c6cd98c221398a3f6db4844e9779
Swetha Vadlakonda <swethv@google.com>
2023-07-11 19:47:22 +0530

    using marshal indent

--- main.go
+++ main.go
@@ -178,10 +178,10 @@ func createStorageHandle(flags *flagStorage) (storageHandle storage.StorageHandl
 
 // Mount the file system according to arguments in the supplied context.
 func mountWithArgs(
-		bucketName string,
-		mountPoint string,
-		flags *flagStorage,
-		mountStatus *log.Logger) (mfs *fuse.MountedFileSystem, err error, storageHandle storage.StorageHandle) {
+	bucketName string,
+	mountPoint string,
+	flags *flagStorage,
+	mountStatus *log.Logger) (mfs *fuse.MountedFileSystem, err error, storageHandle storage.StorageHandle) {
 	// Enable invariant checking if requested.
 	if flags.DebugInvariants {
 		locker.EnableInvariantsCheck()
@@ -230,9 +230,9 @@ func mountWithArgs(
 }
 
 func populateArgs(c *cli.Context) (
-		bucketName string,
-		mountPoint string,
-		err error) {
+	bucketName string,
+	mountPoint string,
+	err error) {
 	// Extract arguments.
 	switch len(c.Args()) {
 	case 1:
@@ -406,7 +406,7 @@ func runCLIApp(c *cli.Context) (err error) {
 
 	logger.Info("Printing the metadata objects length")
 	logger.Info(len(sh.GetMetadataObjects()))
-	j, err := json.Marshal(sh.GetMetadataObjects())
+	j, err := json.MarshalIndent(sh.GetMetadataObjects(), "", " ")
 	if err != nil {
 		logger.Info("received error %s", err)
 	}
