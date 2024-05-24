d75eb4babe3fe25bc8f25a4b60cb3d605bcd6649
Tulsi Shah <tulsishah@google.com>
2024-05-24 05:21:30 +0000

    meta data server testing

--- tools/integration_tests/util/mounting/dynamic_mounting/dynamic_mounting.go
+++ tools/integration_tests/util/mounting/dynamic_mounting/dynamic_mounting.go
@@ -43,7 +43,6 @@ func MountGcsfuseWithDynamicMounting(flags []string) (err error) {
 		"--log-file="+setup.LogFile(),
 		"--log-format=text",
 		"--custom-endpoint=storage.apis-tpczero.goog:443",
-		"--key-file=/tmp/sa.key.json",
 		setup.MntDir())
 
 	for i := 0; i < len(defaultArg); i++ {
