906027459065d4d89bc800e8e55a7e9d1abce3ef
Tulsi Shah <tulsishah@google.com>
2024-05-31 12:03:47 +0000

    updating gsheet

--- perfmetrics/scripts/gsheet/gsheet.py
+++ perfmetrics/scripts/gsheet/gsheet.py
@@ -1,3 +1,4 @@
+import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
@@ -14,9 +15,9 @@ def _get_sheets_service_client():
 
 
 def write_to_google_sheet(worksheet: str, data) -> None:
-
+  spreadsheet_id = SPREADSHEET_ID
   if os.environ.get("MACHINE_TYPE") == "n2-standard-96" :
-    SPREADSHEET_ID = "14-hU4_PWqY6ETSlW-uJPww9psw_usasYbnTTxO7brpc"
+    spreadsheet_id = "14-hU4_PWqY6ETSlW-uJPww9psw_usasYbnTTxO7brpc"
 
   """Calls the API to update the values of a sheet.
 
@@ -31,19 +32,19 @@ def write_to_google_sheet(worksheet: str, data) -> None:
 
   # Getting the index of the last occupied row in the sheet
   spreadsheet_response = sheets_client.spreadsheets().values().get(
-      spreadsheetId=SPREADSHEET_ID,
+      spreadsheetId=spreadsheet_id,
       range='{}!A1:A'.format(worksheet)).execute()
   entries = len(spreadsheet_response['values'])
 
   # Clearing the occupied rows
   request = sheets_client.spreadsheets().values().clear(
-      spreadsheetId=SPREADSHEET_ID,
+      spreadsheetId=spreadsheet_id,
       range='{}!A2:{}'.format(worksheet,entries+1), 
       body={}).execute()
 
   # Appending new rows
   sheets_client.spreadsheets().values().update(
-      spreadsheetId=SPREADSHEET_ID,
+      spreadsheetId=spreadsheet_id,
       valueInputOption='USER_ENTERED',
       body={
           'majorDimension': 'ROWS',
